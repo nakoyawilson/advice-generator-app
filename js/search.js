483133c60151eb0f743bf1b8548ac2f1be97c6b8
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-02-18 20:06:11 -0400

    Add search for advice functionality

---
+++ js/search.js
@@ -0,0 +1,55 @@
+const search = document.querySelector("#search"),
+  searchForm = document.querySelector("#search-form"),
+  searchResults = document.querySelector("#search-results");
+
+const searchAdvice = async (query) => {
+  try {
+    const response = await axios.get(
+      `https://api.adviceslip.com/advice/search/${query}`
+    );
+    const adviceSlips = response.data.slips;
+    searchResults.innerHTML = "";
+    adviceSlips.forEach((slip) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const adviceTitle = document.createElement("span");
+      adviceTitle.classList.add("advice-title");
+      adviceTitle.innerHTML = `Advice #${slip.id}`;
+      card.append(adviceTitle);
+      const divider = document.createElement("picture");
+      divider.classList.add("divider-wrapper");
+      divider.innerHTML = `<source media="(max-width: 719px)"
+      srcset="images/pattern-divider-mobile.svg"/><source
+      media="(min-width: 720px)" srcset="images/pattern-divider-desktop.svg" /><img src="images/pattern-divider-mobile.svg" alt="" class="divider" />`;
+      card.append(divider);
+      const adviceText = document.createElement("p");
+      adviceText.classList.add("advice-text");
+      adviceText.innerHTML = `&ldquo;${slip.advice}&rdquo;`;
+      card.append(adviceText);
+      searchResults.append(card);
+    });
+  } catch (err) {
+    console.log(err);
+    searchResults.innerHTML = "";
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("col-2");
+    const adviceText = document.createElement("p");
+    adviceText.classList.add("advice-text");
+    adviceText.innerHTML = "No advice found";
+    card.append(adviceText);
+    const divider = document.createElement("picture");
+    divider.classList.add("divider-wrapper");
+    divider.innerHTML = `<source media="(max-width: 719px)"
+    srcset="images/pattern-divider-mobile.svg"/><source
+    media="(min-width: 720px)" srcset="images/pattern-divider-desktop.svg" /><img src="images/pattern-divider-mobile.svg" alt="" class="divider" />`;
+    card.append(divider);
+    searchResults.append(card);
+  }
+};
+
+searchForm.addEventListener("submit", (e) => {
+  const searchQuery = search.value;
+  searchAdvice(searchQuery);
+  e.preventDefault();
+});
